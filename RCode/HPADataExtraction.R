1f6ce56846128dfd788c91ca347d446e924f34e0
johan-gson <johan.gson@gmail.com>
2022-07-06 12:39:48 +0200

    Added code for generation of HPA data

---
+++ RCode/HPADataExtraction.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+memory.limit(size = 100000)
+
+fig__dir = "D:/SingleCellModeling/MetAtlas/Figures/"
+
+#use the data.table package, it is better for reading large datasets
+#install.packages("data.table")
+library(data.table)
+Sys.setenv("VROOM_CONNECTION_SIZE" = 500000) #needed to be able to read long lines
+
+d = fread("D:/SingleCellModeling/HPA/rna_single_cell_read_count.tsv")
+dim(d) # 566108  20093
+library(Matrix.utils) #for transpose of sparse matrix
+
+#need to convert to sparse in chunks - will run out of memory otherwise...
+gc()
+sparseMat1 = Matrix(as.matrix(d[1:100000,c(-1,-2,-3)]), sparse=TRUE)
+gc()
+sparseMat2 = Matrix(as.matrix(d[100001:200000,c(-1,-2,-3)]), sparse=TRUE)
+gc()
+sparseMat3 = Matrix(as.matrix(d[200001:300000,c(-1,-2,-3)]), sparse=TRUE)
+gc()
+sparseMat4 = Matrix(as.matrix(d[300001:400000,c(-1,-2,-3)]), sparse=TRUE)
+gc()
+sparseMat5 = Matrix(as.matrix(d[400001:500000,c(-1,-2,-3)]), sparse=TRUE)
+gc()
+sparseMat6 = Matrix(as.matrix(d[500001:(dim(d)[1]),c(-1,-2,-3)]), sparse=TRUE)
+gc()
+
+metaData = d[,1:3]
+rm(d)
+gc()
+
+#merge into 1 matrix
+sparseMat12 = rbind2(sparseMat1,sparseMat2)
+rm(sparseMat1)
+rm(sparseMat2)
+gc()
+sparseMat45 = rbind2(sparseMat4,sparseMat5)
+rm(sparseMat4)
+rm(sparseMat5)
+gc()
+sparseMat123 = rbind2(sparseMat12,sparseMat3)
+rm(sparseMat12)
+rm(sparseMat3)
+gc()
+sparseMat12345 = rbind2(sparseMat123,sparseMat45)
+rm(sparseMat123)
+rm(sparseMat45)
+gc()
+sparseMat123456 = rbind2(sparseMat12345,sparseMat6)
+rm(sparseMat12345)
+rm(sparseMat6)
+gc()
+dim(sparseMat123456)
+sparseMat = sparseMat123456
+rm(sparseMat123456)
+
+
+sparseMat2 = t(sparseMat)
+dim(sparseMat2)
+sparseMat = sparseMat2
+colnames(sparseMat) = metaData$Cell
+saveRDS(sparseMat, "D:/SingleCellModeling/MetAtlas/SparseMat.RDS")
+saveRDS(metaData, "D:/SingleCellModeling/MetAtlas/metaData.RDS")
+
+#Some tests to verify that the matrix is correct (The numbers were looked up manually in the text file)
+#test one number from each chunk
+sparseMat[7,2] == 47 #ok
+sparseMat[3,119510] == 4 #ok
+sparseMat[7,207622] == 2 #ok
+sparseMat[7,312933] == 2 #ok
+sparseMat[1,406090] == 11 #ok
+sparseMat[7,521545] == 3 #ok
+#all looks ok!
+
+rm(sparseMat)
+rm(metaData)
+gc()
+
---
+++ RCode/HPADataGeneration.R
@@ -0,0 +1,114 @@
+library(tidyverse)
+memory.limit(size = 100000)
+
+fig__dir = "D:/SingleCellModeling/MetAtlas/Figures/"
+
+library(Matrix.utils) #for transpose of sparse matrix
+
+sparseMat = readRDS("D:/SingleCellModeling/MetAtlas/SparseMat.RDS")
+metaData = readRDS("D:/SingleCellModeling/MetAtlas/metaData.RDS")
+
+tissClust = metaData[,c(1,3)]
+tissClust2 = unique(metaData[,c(1,3)])
+unique(tissClust2$Tissue)#26 tissues
+
+nm = rep(NA,dim(tissClust2)[1]) #444 in total
+for (i in 1:length(nm))  {
+  print(i)
+  nm[i] = sum(tissClust$Tissue == tissClust2$Tissue[i] & tissClust$Cluster == tissClust2$Cluster[i])
+}
+
+nm #not that bad
+
+
+#sum(nm > 1500) #125
+sum(nm > 400) #297
+#run DSAVE on these and see how they fare, and estimate a minimal number of cells per cluster from that
+library(DSAVE)
+tissClust3 = tissClust2[nm>400,]
+dsaveRes = vector(mode = "list", length = dim(tissClust3)[1])
+for (i in 1:(dim(tissClust3)[1])) {
+  print(i)
+#  dSub = d[d$Tissue == tissClust3$Tissue[i] & d$Cluster == tissClust3$Cluster[i],]
+#  dSub2 = as(as.data.frame(dSub[,c(-1,-2,-3)]),"Matrix")
+  dSub = sparseMat[,metaData$Tissue == tissClust3$Tissue[i] & metaData$Cluster == tissClust3$Cluster[i]]
+  res = DSAVEGetTotalVariationPoolSize(dSub, upperBound = 50, lowerBound = 5e-1, poolSizes = c(100,200,500,750,1000,1500,2000,2500,3000,4000,5000))
+  dsaveRes[[i]] = res
+}
+
+dsBulk = DSAVE::bulkTotalVar1vs1[[4]] [[2]]
+
+saveRDS(dsaveRes, "D:/SingleCellModeling/MetAtlas/DSAVEData.RDS")
+
+
+
+
+#Set the filter manually from the figure
+#We see that we need about 1,000 cells for adipose tissue on average in the figure
+#translate this to UMIs per cluster, and assume it is the same for all
+
+dAdi = sparseMat[,metaData$Tissue == "Adipose"]
+dim(dAdi)#20090 83536
+UMIsPerAdiCell = colSums(dAdi)
+reqUMIsPerClust = mean(UMIsPerAdiCell)*1000#5009333
+
+#Now, go through each cluster and
+# 1) Check if it has enough UMIs
+# 2) Check if it has
+tissClust = metaData[,c(1,3)]
+tissClust2 = unique(tissClust) #444 long
+unique(tissClust2$Tissue)#26 tissues
+
+gc()
+
+UMIsPerClust = rep(NA,dim(tissClust2)[1])
+UMIsPerCell = colSums(sparseMat)
+for (i in 1:length(UMIsPerClust))  {
+  print(i)
+  sel = tissClust$Tissue == tissClust2$Tissue[i] & tissClust$Cluster == tissClust2$Cluster[i]
+  UMIsPerClust[i] = sum(UMIsPerCell[sel])
+}
+sum(UMIsPerClust >= reqUMIsPerClust)
+sel = UMIsPerClust >= reqUMIsPerClust
+tissClustToModel = tissClust2[sel]
+saveRDS(tissClustToModel, "D:/SingleCellModeling/MetAtlas/tissClustToModel.RDS")
+
+#now generate the bootstrap data
+set.seed(1)
+
+for (i in 1:nrow(tissClustToModel)) {
+  print(paste0(i," of ", nrow(tissClustToModel)))
+  sel = tissClust$Tissue == tissClustToModel$Tissue[i] & tissClust$Cluster == tissClustToModel$Cluster[i]
+  subMat = sparseMat[, sel]
+  pooledBootstraps = Matrix(0, nrow = nrow(subMat), ncol=100)
+  rownames(pooledBootstraps) = rownames(subMat)
+  for (j in 1:100) {
+    #bootstrap,i.e. sample the same number of samples with replacement 
+    sel2 = sample(ncol(subMat),ncol(subMat), replace = TRUE) 
+    pooledBootstraps[,j] = rowSums(subMat[,sel2])
+  }
+  tibbBstr = as_tibble(pooledBootstraps) %>% add_column(gene = rownames(pooledBootstraps), .before = 1)
+  sampleName = paste0(tissClustToModel$Tissue[i],'_',tissClustToModel$Cluster[i]);
+  fn = paste0("D:/SingleCellModeling/MetAtlas/Bootstraps/Bootstrap_", sampleName,".txt");
+  write_tsv(tibbBstr, file=fn)
+}
+
+###############################
+#Generate mapping to cell types
+###############################
+
+#Now the cell types
+d = read_tsv("D:/SingleCellModeling/HPA/rna_single_cell_type_tissue.tsv")
+dSub = unique(d[,3:5]) #444, matches perfectly with the counts file, so this most likely worked
+#hmm, the tissues are named differently... annoying...
+ctTissues = unique(dSub$Tissue)
+mdTissues =unique(metaData$Tissue )
+#so, we replace the tissues with the names in the metadata
+print(tibble(ctTissues, mdTissues), n=100) #manually checked them, they come in the same order
+convTab = tibble(x = ctTissues, y = mdTissues)
+#remove "c-" from the cluster
+print(tibble(substring(dSub$Cluster, 3, 100), dSub$Cluster),n=500)#manually checked them, ok
+library(qdapTools)
+d2 = tibble(tissueCT = dSub$Tissue, cluster = substring(dSub$Cluster, 3, 100), celltype = dSub$`Cell type` , tissueMD = lookup(dSub$Tissue, convTab))
+print(d2,n=500) #Looks good. 
+write_tsv(d2, "D:/SingleCellModeling/MetAtlas/tissue_ct.txt")
